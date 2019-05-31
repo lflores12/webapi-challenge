9b2b78409b263dc3fc4517edec892088f8e4a201
Leonel <leonelaf12@gmail.com>
2019-05-31 12:13:59 -0400

    made get and post request and working properly for projects

--- data/Routes/projectRouter.js
+++ data/Routes/projectRouter.js
@@ -7,12 +7,24 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        const projects = await Projects.get(req.query);
+        const projects = await Projects.get();
         res.status(200).json(projects);
     } catch(error) {
         console.log(error);
         res.status(500).json({
             message: "Error retrieving projects"
+        });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const newProject = await Projects.insert(req.body);
+        res.status(201).json(newProject);
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            message: "Error posting project"
         });
     }
 });
