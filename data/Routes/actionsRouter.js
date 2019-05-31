49e4f617988beee92699dd16c942bbfb2737f447
Leonel <leonelaf12@gmail.com>
2019-05-31 13:03:13 -0400

    get and getbyId working on actions

--- data/Routes/actionsRouter.js
+++ data/Routes/actionsRouter.js
@@ -1,9 +1,32 @@
 const express = require('express');
 
-const Projects = require('../helpers/projectModel.js');
 const Actions = require('../helpers/actionModel.js');
 
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try{
+        const actions = await Actions.get();
+        res.status(200).json(actions);
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            message: "Error retrieving actions"
+        });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const action = await Actions.get(req.params.id);
+        res.status(200).json(action);
+    } catch(error) {
+        console.log(error);
+        res.status(404).json({
+            message: "Error retrieving action"
+        });
+    }
+});
 
 
 module.exports = router;
