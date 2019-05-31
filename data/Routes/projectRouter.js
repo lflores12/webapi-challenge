4e7cafdbbe8d127a11b4aa66de6e4be33a316a74
Leonel <leonelaf12@gmail.com>
2019-05-31 11:45:01 -0400

    made routes for actions and projects

---
+++ data/Routes/projectRouter.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const Projects = require('../helpers/projectModel.js');
+const Actions = require('../helpers/actionModel.js');
+
+const router = express.Router();
